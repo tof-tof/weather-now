d64a1f7d9b10167f69323ee512c27150f20c6543
Tofunmi Adigun-Hameed <tofunmi.ah@gmail.com>
2021-02-12 22:35:47 +0000

    array of weathercards working

--- src/Components/WeatherCard.js
+++ src/Components/WeatherCard.js
@@ -3,21 +3,21 @@ import React from "react";
 import compassPoint from "../compassPoint";
 
 
-function WeatherCard(props) {
-  
-  let compassDirection = compassPoint(props.windDirection);
+function WeatherCard({ place }) {
   return (
-    <div className="border w-64 mb-4 rounded overflow-hidden ">
-      <h4 className="text-2xl font-bold">{props.name}, {props.country}</h4>
-      <p>{props.description}</p>
-      <p>{props.temp}<sup>o</sup>C</p>
-      <p>{props.temp_min}<sup>o</sup>C to {props.temp_max}<sup>o</sup>C</p>
-      <p>{props.humidity}%</p>
-      <p>Cloudiness - {props.cloudiness}%</p>
-      <h6 className="text-lg font-semibold">Wind:</h6>  
-      <p>Speed - {props.windSpeed} meter/sec</p>
-      <p>Direction - {props.windDirection}<sup>o</sup> ({compassDirection}) </p>
-    </div>
+    <div className="inline-block px-4 w-64 h-64"> 
+            <div className="bg-blue-500 rounded-lg overflow-hidden shadow-xl my-8 py-4 px-4" key={place.name+place.country}>
+              <h4 className="text-2xl font-bold">{place.name}, {place.country}</h4>
+              <p>{place.description}</p>
+              <p>{place.temp}<sup>o</sup>C</p>
+              <p>{place.temp_min}<sup>o</sup>C to {place.temp_max}<sup>o</sup>C</p>
+              <p>{place.humidity}%</p>
+              <p>Cloudiness - {place.cloudiness}%</p>
+              <h6 className="text-lg font-semibold">Wind:</h6>  
+              <p>Speed - {place.windSpeed} meter/sec</p>
+              <p>Direction - {place.windDirection}<sup>o</sup> ({compassPoint(place.windDirection) })</p>
+            </div>
+          </div>
   )
 };
 
